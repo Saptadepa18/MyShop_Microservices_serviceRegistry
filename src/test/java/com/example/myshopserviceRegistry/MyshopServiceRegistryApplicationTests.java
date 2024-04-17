b888dc549837dbfc4d039bd4fa3bc139aa050912
Saptadeepa Mitra <saptadeepa_mitra@persistent.com>
2024-04-17 15:38:05 +0530

    first commit

---
+++ src/test/java/com/example/myshopserviceRegistry/MyshopServiceRegistryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.myshopserviceRegistry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyshopServiceRegistryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
